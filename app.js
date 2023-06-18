569309b6ea9b2f6bbca9528bb1aecb39b244280b
Rishav <theabstergo06@gmail.com>
2023-06-18 12:49:26 +0530

    Fixing Syntax error

--- app.js
+++ app.js
@@ -35,6 +35,6 @@ app.use("/api/v1", other);
 
 export default app;
 
-res.send(`<h1>Site is Working. click <a href=${process.env.FRONTEND_URL}>here</a> to visit frontend.</h1>`)
-
+app.get("/",(req,res)=>res.send(`<h1>Site is Working. click <a href=${process.env.FRONTEND_URL}>here</a> to visit frontend.</h1>`)
+)
 app.use(ErrorMiddleware)
